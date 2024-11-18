5f5cafbe4e14636305940137988baebcfffb928f
Ethan K <85808692+StrixTyphoon@users.noreply.github.com>
2024-11-18 17:54:15 +0000

    mac support 0.1.0

--- src/extension.ts
+++ src/extension.ts
@@ -2,43 +2,6 @@ import * as vscode from 'vscode';
 import { exec, spawn } from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
-	// const editor = vscode.window.activeTextEditor;
-	// if (editor === undefined){
-	// 	return;
-	// }
-	// else if (editor.document.languageId === 'ask') {
-	// 	vscode.window.showInformationMessage('Active file is a .ask file.');
-	// 	const folderPath = vscode.workspace.rootPath;
-	// 	console.log(folderPath);
-	// 	if (process.platform==="win32") {
-	// 		exec("mkdir '.askignore'", (error, stdout, stderr) => {
-	// 			if (error) {
-	// 				vscode.window.showInformationMessage(`Ask extension inititalised`);
-	// 			}
-	// 			else{	
-	// 			console.log("windows detected");
-	// 			exec(`cd ${folderPath} && cd .askignore && curl -o ask.zip "https://raw.githubusercontent.com/etmclovin/vscode-ask-mode/refs/heads/binary/ask.zip" && tar -xf ask.zip`, (error, stdout, stderr) => {
-	// 				if (error) {
-	// 					vscode.window.showErrorMessage(`Error initialising ask extension: ${stderr}`);
-	// 					return;
-	// 				}
-	// 				const output = stdout;
-	// 			});
-	// 			}
-	// 		});
-	// 	}
-	// 	else if (process.platform === "darwin"){
-			
-	// 	}
-	// 	else{
-	// 		const osPlatform = "linux";
-	// 	}
-	// }
-	// else{
-	// 	return;
-	// }
-
-
 	console.log('ask-mode is now active!');
 
 	const runCommand = vscode.commands.registerCommand('ask-mode.runCommand', () => {
@@ -53,22 +16,52 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.window.showErrorMessage('Active file is not a .ask file.');
             return;
         }
-
+		const folderPath = vscode.workspace.rootPath;
 		const filePath = editor.document.uri.fsPath;
 		editor.document.save();
-		exec(`cd ask && ask "${filePath}"`, (error, stdout, stderr) => {
-			if (error) {
-				vscode.window.showErrorMessage(`Error running Ask script: ${stderr}`);
-				return;
-			}
-			const output = stdout;
-		
-			editor.edit(builder => {
-				const doc = editor.document;
-				builder.replace(new vscode.Range(doc.lineAt(0).range.start, doc.lineAt(doc.lineCount - 1).range.end), output);
-			});
-		});
-});
+		if (process.platform === "win32") {
+			exec(`cd ${folderPath}/.askignore && ask "${filePath}"`, (error, stdout, stderr) => {
+				if (error) {
+					vscode.window.showErrorMessage(`Error running Ask script: ${stderr}`);
+					return;
+				}
+				const output = stdout;
+
+				editor.edit(builder => {
+					const doc = editor.document;
+					builder.replace(new vscode.Range(doc.lineAt(0).range.start, doc.lineAt(doc.lineCount - 1).range.end), output);
+				});
+				});
+		}
+		else if (process.platform === "darwin") {
+			exec(`cd ${folderPath}/.askignore && ./ask "${filePath}"`, (error, stdout, stderr) => {
+				if (error) {
+					vscode.window.showErrorMessage(`Error running Ask script: ${stderr}`);
+					return;
+				}
+				const output = stdout;
+
+				editor.edit(builder => {
+					const doc = editor.document;
+					builder.replace(new vscode.Range(doc.lineAt(0).range.start, doc.lineAt(doc.lineCount - 1).range.end), output);
+				});
+				});
+		}
+		else if (process.platform === "linux") {
+			exec(`cd ${folderPath}/.askignore && ask "${filePath}"`, (error, stdout, stderr) => {
+				if (error) {
+					vscode.window.showErrorMessage(`Error running Ask script: ${stderr}`);
+					return;
+				}
+				const output = stdout;
+
+				editor.edit(builder => {
+					const doc = editor.document;
+					builder.replace(new vscode.Range(doc.lineAt(0).range.start, doc.lineAt(doc.lineCount - 1).range.end), output);
+				});
+				});
+		}
+	});
 	const setupCommand = vscode.commands.registerCommand("ask-mode.setupCommand", () =>{
 		vscode.window.withProgress({
             location: vscode.ProgressLocation.Notification,
@@ -76,8 +69,9 @@ export function activate(context: vscode.ExtensionContext) {
             cancellable: false
         }, async (progress) => {
             try {
+				const folderPath = vscode.workspace.rootPath;
 				if (process.platform === "win32") {
-					const child = spawn('powershell.exe', ['-c', 'mkdir ask ; cd ask ; curl -o ask.zip "https://raw.githubusercontent.com/etmclovin/vscode-ask-mode/refs/heads/binary/ask.zip" ; tar -xf ask.zip ; del ask.zip']);
+					const child = spawn('powershell.exe', ['-c', `cd ${folderPath} ; mkdir .askignore ; cd .askignore ; curl -o ask.zip "https://raw.githubusercontent.com/etmclovin/vscode-ask-mode/refs/heads/binary/ask.zip" ; tar -xf ask.zip ; del ask.zip`]);
 
 					child.stdout.on('data', (data: any) => {
 						console.log(`Powershell Output: ${data}`);
@@ -91,7 +85,7 @@ export function activate(context: vscode.ExtensionContext) {
 					vscode.window.showInformationMessage('Ask installed successfully!');
 				}
 				else if (process.platform === "darwin") {
-					exec(`mkdir ask ; cd ask ; curl -o ask.zip "https://raw.githubusercontent.com/etmclovin/vscode-ask-mode/refs/heads/binary/mac.zip ; tar -xf ask.zip ; rm ask.zip`, (error, stdout, stderr) => {
+					exec(`cd ${folderPath} ; mkdir .askignore ; cd .askignore ; curl -o ask.zip "https://raw.githubusercontent.com/etmclovin/vscode-ask-mode/refs/heads/binary/mac.zip" ; tar -xf ask.zip ; rm ask.zip`, (error, stdout, stderr) => {
 						if (error) {
 							vscode.window.showErrorMessage(`Error installing ask: ${stderr}`);
 							return;
@@ -99,7 +93,7 @@ export function activate(context: vscode.ExtensionContext) {
 					});
 				}
 				else if (process.platform === "linux") {
-					exec(`mkdir ask ; cd ask ; curl -o ask.zip "https://raw.githubusercontent.com/etmclovin/vscode-ask-mode/refs/heads/binary/linux.zip ; tar -xf ask.zip ; rm ask.zip`, (error, stdout, stderr) => {
+					exec(`cd ${folderPath} ; mkdir .askignore ; cd .askignore ; curl -o ask.zip "https://raw.githubusercontent.com/etmclovin/vscode-ask-mode/refs/heads/binary/linux.zip" ; tar -xf ask.zip ; rm ask.zip`, (error, stdout, stderr) => {
 						if (error) {
 							vscode.window.showErrorMessage(`Error installing ask: ${stderr}`);
 							return;
